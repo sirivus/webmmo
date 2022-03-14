2d60601374ec0209aef6cf01138eb17e63875515
siri <louishsu13@gmail.com>
2022-03-14 16:27:09 -0700

    ss

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,13 @@ module.exports = {
 
         use: ["style-loader", "css-loader"],
       },
+            {
+
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: 'asset/resource',
+
+      },
     ],
   },
 };

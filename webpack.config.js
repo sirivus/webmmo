4335b7dc76fcd9244ab6302a52960974b3b41121
siri <louishsu13@gmail.com>
2022-03-14 16:03:33 -0700

    aa

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,6 @@
 const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-    }),
-  ],
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
@@ -19,15 +11,8 @@ module.exports = {
       {
         test: /\.css$/i,
 
-        use: ["MiniCssExtractPlugin.loader, style-loader", "css-loader"],
+        use: ["style-loader", "css-loader"],
       },
-    ],
-  },
-  optimization: {
-    minimizer: [
-      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-      // `...`,
-      new CssMinimizerPlugin(),
     ],
   },
 };
